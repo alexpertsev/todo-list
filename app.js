4fe57be5a604f43be1cc04c5baada0538841de7c
Alex Pertsev <alex.pertsev@canadadrugs.com>
2017-10-20 13:58:12 -0500

    Add more tests

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-    res.send('OK!');
-});
+app.get('/categories', function(req, res) {
+	res.json({'Dairy':['Milk','Eggs','Yogurt']});	
+})
 
 module.exports = app;
--- test.js
+++ test.js
@@ -3,11 +3,33 @@ var app = require('./app');
 
 describe('root path', function() {
 	it('returns 200', function(done) {
-	   request(app).get('/')
-		           .expect(200)
-		           .end(function(error) {
-					   if (error) throw error;
-					   done();
-		           }) 
+		request(app).get('/').expect(200, done)
 	})
-})
+
+	it('returns HTML', function(done) {
+		request(app).get('/').expect('Content-Type', /html/, done)
+	})
+	
+	it('returns shopping categories', function(done) {
+		request(app).get('/').expect(/shopping categories/i, done)
+	})
+});
+
+// [ category_name : [item1, item2, ...], ... ]
+describe('listing /categories', function() {
+	it('returns 200', function(done) {
+		request(app).get('/categories').expect(200, done)
+	})
+
+	it('returns JSON', function(done) {
+		request(app).get('/categories')
+			.expect('Content-Type', /json/, done)
+	})
+
+	it('returns initial categories', function(done) {
+		request(app).get('/categories')
+			.expect(JSON.stringify({
+				'Dairy': ['Milk', 'Eggs', 'Yogurt']
+			}), done)
+	})
+});
